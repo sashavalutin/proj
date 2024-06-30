17e4a61b6df539646876532f6f0a0cbf77598051
Avalutin <sashavalutin@yandex.ru>
2024-06-30 23:57:32 +0300

    Изменена основная логика и изменён метод doSomething

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -4,7 +4,11 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("1001");
-        System.out.println("222");
-        System.out.println("333");
+    }
+
+    public static void doSomething() {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
     }
 }
