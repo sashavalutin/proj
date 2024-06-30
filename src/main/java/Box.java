3e7a32465a24ec9ea6833de133c8320aae809d0b
Avalutin <sashavalutin@yandex.ru>
2024-06-30 23:55:08 +0300

    Изменена основная логика и бобавлен класс коробки

---
+++ src/main/java/Box.java
@@ -0,0 +1,19 @@
+public class Box {
+    private  int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Box() {
+
+    }
+
+    public Box(int size) {
+        this.size = size;
+    }
+}
--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -3,6 +3,8 @@
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("1111111");
+        System.out.println("1001");
+        System.out.println("222");
+        System.out.println("333");
     }
 }
