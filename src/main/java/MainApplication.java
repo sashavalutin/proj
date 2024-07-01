079b0a85ea5c155ed926ea8ca360cb7a4ed9bfd5
Avalutin <sashavalutin@yandex.ru>
2024-07-01 07:09:37 +0300

    Изменили локинку main и doSomething

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -2,13 +2,15 @@
 
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println("1001");
         System.out.println("Hello World");
         System.out.println("1001");
+
     }
 
     public static void doSomething() {
+        System.out.println(0);
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(1);
     }
 }
