a649d94919a8307974d551839e3e369b41e1204f
Avalutin <sashavalutin@yandex.ru>
2024-07-01 23:28:07 +0300

    добавлен вызов метода doSomething

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -2,9 +2,10 @@
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("1001");
+        System.out.println("---");
         System.out.println("Hello World");
         System.out.println("1001");
+        doSomething();
         System.out.println("----");
     }
 
